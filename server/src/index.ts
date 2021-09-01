bd871eed47f3e75d584f0436ff370ca46d04b718
Ross Bratton <RossB0918@randox.com>
2021-09-01 16:21:10 +0100

    Updated to change server to typescript and implement nodemon/tsc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({ message: 'Base API called.' });

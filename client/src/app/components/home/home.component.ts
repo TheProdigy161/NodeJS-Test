6e4d1401d52d10b353b31d9a832698c10216236a
Ross Bratton <RossB0918@randox.com>
2021-09-01 16:26:18 +0100

    Updated to add home screen to client

---
+++ client/src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+import { ApiService } from './../../services/api.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  call$: Observable<any> = new Observable();
+
+  constructor(public api: ApiService) { }
+
+  ngOnInit(): void {
+    this.call$ = this.api.get("http://localhost:5000");
+  }
+}

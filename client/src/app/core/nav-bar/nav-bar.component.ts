4a223d174169a0c596aea84f8e2e7e95b9f0b098
Ross Bratton <RossB0918@randox.com>
2021-11-25 14:29:50 +0000

    Updated to try and add animations to sidenav

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -3,11 +3,24 @@ import { VersionData } from '@models/versionData/versionData.model';
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  animations: [
+    trigger('expandSidenav', [
+      state('normal', style({
+        // opacity: 1
+      })),
+      state('expand', style({
+        // opacity: 0
+      })),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ])
+  ]
 })
 export class NavBarComponent implements OnInit {
   version$: Observable<VersionData> = new Observable();
@@ -22,5 +35,9 @@ export class NavBarComponent implements OnInit {
 
   toggleSidenav(): void {
     this.showOnlyIcon = !this.showOnlyIcon;
+  }
+
+  getStateName(): string {
+    return this.showOnlyIcon ? 'normal' : 'expand'
   }
 }

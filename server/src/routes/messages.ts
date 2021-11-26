c1158b74eafe207d44da8684c05c1e04539bc1d1
Ross Bratton <RossB0918@randox.com>
2021-11-26 11:13:55 +0000

    Updated message temp data

--- server/src/routes/messages.ts
+++ server/src/routes/messages.ts
@@ -10,21 +10,21 @@ export default function (app: Express): void {
                 id: 1,
                 title: 'First message',
                 subtitle: 'First message subtitle.',
-                content: 'This is my first message.',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id fermentum ante, et mollis tortor. In varius quam eu venenatis bibendum. Integer interdum eleifend purus sit amet cursus. In quis porttitor mauris, vitae luctus nisi. Sed in neque dapibus, iaculis eros vel, commodo massa. Etiam dui enim, ultricies vel feugiat vel, aliquet vitae dui. Proin viverra leo non metus fringilla, sit amet venenatis est dapibus. Duis porttitor odio urna, consequat sodales lectus dapibus non. Nullam eu rhoncus metus. Etiam hendrerit molestie pharetra. Nullam ornare neque nunc, sed mollis quam varius in. Quisque ultrices nibh ligula, ut maximus nibh semper at. Duis facilisis et leo lobortis facilisis. Curabitur neque tellus, lacinia et dolor et, fermentum volutpat elit.',
                 auditInfo: null
             }),
             new Message({
                 id: 2,
                 title: 'Second message',
                 subtitle: 'Second message subtitle.',
-                content: 'This is my second message.',
+                content: 'Vivamus laoreet ut nisl sit amet aliquam. Mauris massa magna, feugiat vitae elit id, rutrum consequat quam. Donec ultrices sodales leo. Donec dapibus dolor eget lorem tincidunt, suscipit dictum risus rhoncus. Aenean pretium diam vel nulla sagittis vestibulum. Quisque sit amet turpis tempor, malesuada arcu ullamcorper, euismod tellus. Fusce vitae felis ut orci dignissim dapibus malesuada vel eros. Morbi pretium, diam id elementum ullamcorper, erat nulla malesuada eros, eu tempus diam nunc ut justo. Vestibulum vitae nisi vitae nulla ullamcorper placerat.',
                 auditInfo: null
             }),
             new Message({
                 id: 3,
                 title: 'Third message',
                 subtitle: 'Third message subtitle.',
-                content: 'This is my third message.',
+                content: 'Ut bibendum erat vitae libero eleifend pulvinar. Fusce pretium tempor neque venenatis tempus. Praesent lobortis magna nec erat fringilla, vel semper leo volutpat. Cras viverra quis lacus ut semper. Morbi tincidunt bibendum tortor nec pretium. Curabitur id sapien leo. Vestibulum vitae magna dapibus, fermentum lacus eu, venenatis ex. Nullam non enim rutrum, tincidunt neque quis, sagittis risus.',
                 auditInfo: null
             }),
         ];
